03f343fc2878ae571117b19774a9585d5c9caa9f
mali8 <belsak@gmail.com>
2018-05-04 13:48:53 +0200

    C:/Program Files/Git/parse path for api with blank parse method

--- msisdnServer.go
+++ msisdnServer.go
@@ -9,6 +9,7 @@ const indexBody = `
 <html>
   <head>
 	<title>PhoneNumberServer</title>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   </head>
   <body>
 	<form>
@@ -19,6 +20,17 @@ const indexBody = `
 	<div id="results">
 	</div>
   </body>
+  <script>
+    $("form").submit(function(e){
+		event.preventDefault();
+		$.ajax({
+			"url": "/parse?" + $("form").serialize(), 
+			"success": function(data, status, xhr){
+				$("#results").prepend("<div>" + $("#phoneNumber").val() + "</div>");
+			}
+		});
+	})
+  </script>
 </html>
 `
 
@@ -27,8 +39,16 @@ func index(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(indexBody))
 }
 
+func parse(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	phoneNumber := r.Form.Get("phoneNumber")
+
+	log.Printf("Phone number to be parsed %s", phoneNumber)
+}
+
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/parse", parse)
 	log.Println("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
